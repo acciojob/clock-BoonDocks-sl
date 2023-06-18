a6aa203d946e138d424f991de29d0f7052973907
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-18 21:39:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+ function updateTimer() {
+      var timerElement = document.getElementById("timer");
+      var currentTime = new Date();
+
+      var date = currentTime.toLocaleDateString();
+      var time = currentTime.toLocaleTimeString();
+
+      timerElement.textContent = date + ", " + time;
+    }
+
+    setInterval(updateTimer, 1000);
